351e199d1c192a3b4e8b28fa7fb9a2bb5ed670a8
RyanByrneLaptop <C17326283@mydit.ie>
2019-04-01 11:30:53 +0100

    Control made

---
+++ PROJECT/src/com/assignment/machinelearning/Control.java
@@ -0,0 +1,12 @@
+package com.assignment.machinelearning;
+
+public class Control
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		System.out.print("Hello World");
+	}
+
+}
